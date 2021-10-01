a46fc4f04ade44d57fd09e1ac1704ac8e964af8c
Felipe Lozano <felipelozano@iMac-de-Felipe.local>
2021-09-30 19:42:30 -0500

    Apple Config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,8 +8,8 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // add your google maps key
-    GMSServices.provideAPIKey("AIzaSyDdyth2EiAjU9m9eE_obC5fnTY1yeVNTJU")
+    // add your google maps key 
+    GMSServices.provideAPIKey("AIzaSyCNwco3d7XT6XUvoLAxUqYDnkPiLaw8ldw")
     FirebaseApp.configure() //add this before the code below
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
